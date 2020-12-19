d0ba69094f03f08495737d11417666ca540c84d0
89oinotna <a.z.zegarelli@live.it>
2020-12-19 21:52:20 +0100

    -opa type (now use binop)

--- microc-semantic-analysis/src/ast.ml
+++ microc-semantic-analysis/src/ast.ml
@@ -6,7 +6,6 @@ type binop = Add | Sub | Mult | Div  | Mod | Equal | Neq | Less | Leq |
 
 type uop = Neg | Not | PreInc | PreDec | PostInc | PostDec [@@deriving show]
 
-type opa= Add | Sub | Div | Mult | Mod [@@deriving show]
 
 type identifier = string [@@deriving show]
 
@@ -27,7 +26,7 @@ type typ =
 and expr =  expr_node annotated_node                                                   
 and expr_node =     
   | Access of access                 (* x    or  *p    or  a[e]     *) 
-  | OpAssign of opa * access * expr  (* x+=e x-=e ...               *)
+  | OpAssign of binop * access * expr  (* x+=e x-=e ...               *)
   | Assign of access * expr          (* x=e  or  *p=e  or  a[e]=e   *)
   | Addr of access                   (* &x   or  &*p   or  &a[e]    *)
   | ILiteral of int                  (* Integer literal             *)
